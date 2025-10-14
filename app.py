10c456c7d67db0c0a564c980ed91538b0187239a
EvinRobin4 <evinrobin35@gmail.com>
2025-10-14 21:07:10 +0530

    Update app.py for Render deployment (host and port)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -73,5 +74,9 @@ def delete_task(id): # this is the function to delete task
     conn.close()
     return redirect(url_for('index'))
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)  
+
